73334d3481967ba67d37e41d7462768cd55235f0
José Maria Macedo <jose_macedo@live.com>
2022-06-11 00:04:11 -0300

    Utilizando o comando Input
    Utilizando o comando input

--- Primeiros Codigos.py
+++ Primeiros Codigos.py
@@ -1,7 +1,17 @@
-nome = "josé"
-idade = 25
-sobrenome = "macedo"
+#nome = "josé"
+#idade = 25
+#sobrenome = "macedo"
 
 #print(nome, sobrenome, idade)
 
-print("Olá", nome, sobrenome, "sua idade é", idade)
+#print("Olá", nome, sobrenome, "sua idade é", idade)
+
+#print(f"Olá, {nome} {sobrenome}, sua idade é {idade}")
+
+#Aprendendo a utilizar o comando Input
+
+nome2 = input("Digite o seu nome: ")
+sobrenome2 = input("Digite seu sobrenome: ")
+idade2 = input("Digite seu sobrenome: ")
+
+print(f"Olá {nome2} {sobrenome2}, sua idade é {idade2}")
