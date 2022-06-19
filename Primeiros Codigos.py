5c48c85641f77e553d393a474ccbbe6fc39b1ebb
José Maria Macedo <jose_macedo@live.com>
2022-06-19 16:39:45 -0300

    atribuindo operadores
    Realizando operações simples

--- Primeiros Codigos.py
+++ Primeiros Codigos.py
@@ -1,23 +1,34 @@
-#nome = "josé"
-#idade = 25
-#sobrenome = "macedo"
+nome = "josé"
+idade = 25
+sobrenome = "macedo"
 
 #print(nome, sobrenome, idade)
 
 #print("Olá", nome, sobrenome, "sua idade é", idade)
 
-#print(f"Olá, {nome} {sobrenome}, sua idade é {idade}")
+print(f"Olá, {nome} {sobrenome}, sua idade é {idade}")
 
 #Aprendendo a utilizar o comando Input
 
 nome2 = input("Digite o seu nome: ")
 sobrenome2 = input("Digite seu sobrenome: ")
-idade2 = int(input("Digite seu sobrenome: "))
+idade2 = int(input("Digite sua idade: "))
 
 print(f"Olá {nome2} {sobrenome2}, sua idade é {idade2}")
 
 #Operadores
 
-print (10 + 10) #Resultado = 20
-print (10 – 5) # Resultado = 5
-print(10 * 5) # Resultado = 50
+soma_idade = idade + idade2
+
+print(f"A soma da idades é {soma_idade}")
+
+subtracao_idade = idade - idade2
+print(f"A subtração das idades é {subtracao_idade}")
+
+multiplicacao_idade = idade * idade2
+print(f"A multiplicacao das idades é {multiplicacao_idade}")
+
+divisao_idade = idade / idade2
+print(f"A divisão das idades é {divisao_idade}")
+
+
